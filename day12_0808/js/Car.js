ac5cbb9ee0c4586ea86fe4a9101a5c457b33575e
gxnny19 <wkdrudrjs010@gmail.com>
2025-08-08 16:56:44 +0900

    react

---
+++ day12_0808/js/Car.js
@@ -0,0 +1,9 @@
+ function Car(prop) {
+            return (
+                <>
+                    <h2>안녕.. 마이 빵빵이의 색상은 {pr.color}</h2>
+                    <p>--{prop.name}--우리도 안전운전~~!!</p>
+                </>
+            )
+        }
+        export default Car
